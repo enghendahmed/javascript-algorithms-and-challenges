a316717f30eaca952ed395a1e44b8c774ba55464
enghendahmed <eng.hendahmed2022@gmail.com>
2026-06-06 01:18:40 +0300

    for of loop Using Destructuring

--- 01-concepts/02-arrays/for-of-loop/for-of-loop.js
+++ 01-concepts/02-arrays/for-of-loop/for-of-loop.js
@@ -23,3 +23,25 @@ const restaurant = {
     },
   },
 };
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+for (const item of menu) console.log(item);
+
+// old way before destructuring
+/*
+for (const item of menu.entries()) {
+  //console.log(item);
+  console.log(`${item[0] + 1}:  ${item[1]} `);
+}
+
+*/
+
+// log it just to see what inside it
+//console.log([...menu.entries()]);
+
+// new way Using Destructuring
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1} : ${el}`);
+}
