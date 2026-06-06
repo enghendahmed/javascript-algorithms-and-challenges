f34bfed945bd89887739dc061b70ab8bfc5d017f
enghendahmed <eng.hendahmed2022@gmail.com>
2026-06-06 22:47:13 +0300

    ES6 enhancement : Computed Property Name

---
+++ 01-concepts/03-objects/enhanced-object-literals/README.md
@@ -0,0 +1,13 @@
+# ES6 Enhancement
+
+## 1. ES6 enhanced object literals :
+
+      - inside our object , we just write a property name to refer to an outside object . it should be the same name of the object.
+
+## 2. Writting methods or functions :
+
+      - ES6 allows us to write fn without function keyword and without (:) colon . so the property name then the parenthes ( , ) of the fn or the method. which is easy syntax.
+
+## 3. Compute property name :
+
+      -Compute the property names instead of having to write them out manually and literally
--- 01-concepts/03-objects/enhanced-object-literals/enhanced-object-literals.js
+++ 01-concepts/03-objects/enhanced-object-literals/enhanced-object-literals.js
@@ -1,17 +1,24 @@
 "use strict";
 
+// 1. Es6 enhancement : just write the outside object name as it is a property name
 //we have a separated object ouside the restaurant object But we need openingHours inside restaurant object
+// we change this object name from openingHours into hours
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-const openingHours = {
-  thu: {
+const hours = {
+  [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  fri: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
-  sat: {
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+  [`day-${2 + 4}`]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -24,9 +31,13 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
   //openingHours: openingHours,
-  // (the enhancement is just write it once because the property name is the same object name )
+  // (the enhancement is just write it once because the property name is the same object name ) if we change the object name , we must change the property name
 
   // ES6  enhanced Object literals
-  openingHours,
+  hours,
 };
 console.log(restaurant);
+
+// 2. Es6 enhancement : writting methods
+
+// 3. Es6 enhancement : Compute the property names instead of having to write them out manually and literally
